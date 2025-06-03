d2037b6f4652194a7b2a90446644d499bb258a98
sungbinoh <wwera6421@gmail.com>
2025-06-03 19:13:08 +0000

    updating spack and xrootd setup

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@
 export machine=${HOSTNAME}
 if [[ $machine == *sbnd* || $machine == *jupyter* ]]; then
   echo "working on a sbnd machine"
-  source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
+  source /cvmfs/larsoft.opensciencegrid.org/spack-v0.22.0-fermi/setup-env.sh
   export CAFPYANA_GRID_OUT_DIR="/pnfs/sbnd/scratch/users/$USER/cafpyana_out"
   mkdir -p $CAFPYANA_GRID_OUT_DIR
 fi
 if [[ $machine == *icarus* ]]; then
   echo "working on a icarus machine"
-  source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
+  source /cvmfs/larsoft.opensciencegrid.org/spack-v0.22.0-fermi/setup-env.sh
   export CAFPYANA_GRID_OUT_DIR="/pnfs/icarus/scratch/users/$USER/cafpyana_out"
   mkdir -p $CAFPYANA_GRID_OUT_DIR
 fi
-spack load hdf5@1.14.3
-spack load xrootd@5.6.1
+spack load hdf5@1.14.3%gcc@12.2.0 arch=linux-almalinux9-x86_64_v3
+spack load xrootd@5.6.9%gcc@12.2.0
 
 VENV_NAME=venv_py39_cafpyana
 source ./envs/$VENV_NAME/bin/activate
