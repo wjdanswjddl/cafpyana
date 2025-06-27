280eb706b4c29088cfb681875a803e064e0a9841
afropapp13 <afropapp@yahoo.gr>
2025-06-27 13:40:28 -0500

    June 27 2025: adding/fixing proxy authentication for grid submission

--- setup.sh
+++ setup.sh
@@ -22,3 +22,5 @@ source ./envs/$VENV_NAME/bin/activate
 export PYTHONPATH=$PYTHONPATH:$PWD/..
 export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib/python3.9/site-packages/xrootd-5.6.1-py3.9-linux-x86_64.egg/pyxrootd:$LD_LIBRARY_PATH
 export CAFPYANA_WD=`pwd`
+
+htgettoken -a htvaultprod.fnal.gov -i sbnd
