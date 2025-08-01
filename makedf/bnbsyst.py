e99b84fd8beb198698c6a19ce27fb4a455ef27c2
Mun Jung Jung <munjung@sbndbuild03.fnal.gov>
2025-08-01 17:23:02 -0500

    calcate the product of bnb syst when loading syst

--- makedf/bnbsyst.py
+++ makedf/bnbsyst.py
@@ -1,4 +1,5 @@
 from . import getsyst
+import pandas as pd
 
 # Regen systematic variations
 regen_systematics = [
@@ -17,6 +18,21 @@ regen_systematics = [
     'piplus_Flux'
 ]
 
-def bnbsyst(f, nuind):
-    return getsyst.getsyst(f, regen_systematics, nuind)
+def bnbsyst(f, nuind, multisim_nuniv=250):
+    bnbwgtdf = getsyst.getsyst(f, regen_systematics, nuind, multisim_nuniv=multisim_nuniv)
+
+    # multiply all knobs and save to "Flux.univ_"
+    # dummy df to hold the product of all syst knobs -- iterative inserting causes PerformanceWarning
+    flux_cols = pd.MultiIndex.from_product(
+        [["Flux"], [f"univ_{i}" for i in range(multisim_nuniv)]],
+    )
+    flux_wgt = pd.DataFrame(
+        1.0,
+        index=bnbwgtdf.index,
+        columns=flux_cols,
+    )
+    for syst in regen_systematics:
+        flux_wgt *= bnbwgtdf[syst].to_numpy()
+    bnbwgtdf = pd.concat([bnbwgtdf, flux_wgt], axis=1)
+    return bnbwgtdf
 
