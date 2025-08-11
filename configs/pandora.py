8334b6fa751f8541e3f1fe16347b40a7b117d507
afropapp13 <afropapp@yahoo.gr>
2025-08-11 10:42:00 -0500

    Aug 11 2025: rm mcprimdf from pandora.py

--- configs/pandora.py
+++ configs/pandora.py
@@ -1,4 +1,4 @@
 from makedf.makedf import *
 
-DFS = [make_pandora_df, make_hdrdf, make_potdf_bnb, make_mcnudf, make_mcprimdf]
-NAMES = ["evt", "hdr", "pot", "mcnu" , "mcprim"]
+DFS = [make_pandora_df, make_hdrdf, make_potdf_bnb, make_mcnudf]
+NAMES = ["evt", "hdr", "pot", "mcnu" ]
