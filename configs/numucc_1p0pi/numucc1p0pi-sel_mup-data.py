5089d3e3a14f8ac83e15d426c0caec653fed0123
Mun Jung Jung <munjung@sbndbuild03.fnal.gov>
2026-04-23 17:06:05 -0500

    configs to save all evts

--- configs/numucc_1p0pi/numucc1p0pi-sel_mup-data.py
+++ configs/numucc_1p0pi/numucc1p0pi-sel_mup-data.py
@@ -2,5 +2,6 @@
 # use for detector variation samples
 from analysis_village.numucc_1p0pi.makedf.makedf import *
 
-DFS = [make_pandora_evtdf_mup, make_hdrdf, make_potdf_bnb]
-NAMES = ["evt", "hdr", "bnbpot"]
+DFS = [make_pandora_evtdf_mup, make_hdrdf, make_potdf_bnb, make_triggerdf]
+ARGS = [{}, {}, {}, {}]
+NAMES = ["evt", "hdr", "bnbpot", "trigger"]
--- configs/numucc_1p0pi/sel_all-data.py
+++ configs/numucc_1p0pi/sel_all-data.py
@@ -1,6 +1,7 @@
-# all events for data files
-# use for event selection comparison
+# selected event rates for MC files
+# use for detector variation samples
 from analysis_village.numucc_1p0pi.makedf.makedf import *
 
-DFS = [make_pandora_evtdf_all, make_trkdf, make_hdrdf, make_potdf_bnb]
-NAMES = ["evt", "trk", "hdr", "bnbpot"]
+DFS = [make_pandora_evtdf_all, make_trkdf, make_hdrdf, make_potdf_bnb, make_triggerdf]
+ARGS = [{}, {}, {}, {}, {}]
+NAMES = ["evt", "trk", "hdr", "bnbpot", "trigger"]
