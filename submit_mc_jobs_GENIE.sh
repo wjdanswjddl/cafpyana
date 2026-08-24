f958c6976804515a8c0684df5d945f6a3f366eb6
Mun Jung Jung <munjung@sbndbuild03.fnal.gov>
2026-08-24 12:46:57 -0500

    genie job sub update

--- submit_mc_jobs_GENIE.sh
+++ submit_mc_jobs_GENIE.sh
@@ -1,21 +1,27 @@
-#inputdir=/exp/sbnd/app/users/munjung/misc/filelists/MC/SBND/2025Spring_v10_06_00_09/BNB_cosmics
-#list="${inputdir}/mc_MCP2025C_1e20_v10_06_00_09_prodgenie_corsika_proton_rockbox_sbnd_CV_caf_flat_caf_sbnd_xrootd.list"
+# GENIE knob-group dfs via configs/numucc_1p0pi/sel_mup-geniewgts-knobgroups.py
+# Knob lists: makedf.geniesyst.GENIE_KNOB_GROUPS
+# Valid GENIE_KNOB_GROUP keys: CCQE ZExp MEC RES nonRES DIS Other Ar23p
 #
-#for g in MEC RES nonRES DIS Other; do
-#  GENIE_KNOB_GROUP=$g python run_df_maker.py \
-#    -c configs/numucc_1p0pi/sel_mup-geniewgts-knobgroups.py \
-#    -l "$list" \
-#    -o "sel_mup-wgts_genie_${g}" \
-#    -ngrid 1000
-#done
+# Spring CV sample for standard GENIE groups; Ar23+ respin for Ar23p-only knobs.
 
-inputdir=/exp/sbnd/app/users/munjung/misc/filelists/MC/SBND/Ar23+
-list="${inputdir}/ar23p_respin-xrootd.list"
+spring_inputdir=/exp/sbnd/app/users/munjung/misc/filelists/MC/SBND/2025Spring_v10_06_00_09/BNB_cosmics
+spring_list="${spring_inputdir}/mc_MCP2025C_1e20_v10_06_00_09_prodgenie_corsika_proton_rockbox_sbnd_CV_caf_flat_caf_sbnd_xrootd.list"
+
+for g in CCQE ZExp MEC RES nonRES DIS Other; do
+  GENIE_KNOB_GROUP=$g python run_df_maker.py \
+    -c configs/numucc_1p0pi/sel_mup-geniewgts-knobgroups.py \
+    -l "$spring_list" \
+    -o "sel_mup-wgts_genie_${g}" \
+    -ngrid 1000
+done
+
+ar23_inputdir=/exp/sbnd/app/users/munjung/misc/filelists/MC/SBND/Ar23+
+ar23_list="${ar23_inputdir}/ar23p_respin-xrootd.list"
 
 for g in Ar23p; do
   GENIE_KNOB_GROUP=$g python run_df_maker.py \
     -c configs/numucc_1p0pi/sel_mup-geniewgts-knobgroups.py \
-    -l "$list" \
+    -l "$ar23_list" \
     -o "sel_mup-wgts_genie_${g}" \
     -ngrid 4000
 done
